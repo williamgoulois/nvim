2cfe198fb45ba2022f99bd26f4f5058ac5538e76
William Goulois <william.goulois@fabernovel.com>
2024-06-05 23:37:15 +0200

    feat: change icon for supermaven

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,13 +7,25 @@ return {
         require("supermaven-nvim").setup({})
       end,
     },
+    {
+      "onsails/lspkind.nvim",
+    },
   },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    local lspkind = require("lspkind")
     table.insert(opts.sources, 1, {
       name = "supermaven",
       group_index = 1,
       priority = 100,
     })
+    opts.formatting = {
+      format = require("lspkind").cmp_format({
+        mode = "symbol",
+        max_width = 50,
+        symbol_map = { Supermaven = "" },
+      }),
+    }
+    vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
   end,
 }
